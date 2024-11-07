eaa3593901bc3633a1a01de142863c65f84f7dbe
Lucas Vieira <lucas.vieira94@outlook.com>
2024-11-07 07:41:27 -0300

    chore: fix unit test coverage issues

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,5 +1,8 @@
 """unit tests."""
 
+from poetry_bundle_local_deps_plugin import BundleLocalDependenciesPlugin
 
-def test_placeholder():
-    pass
+
+def test_should_be_defined():
+    """Check if plugin is defined."""
+    assert BundleLocalDependenciesPlugin is not None
